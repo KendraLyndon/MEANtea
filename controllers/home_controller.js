9e5e7f70f243b850da308974a651fe5b089a296c
Kendra Lyndon <kendralyndon@gmail.com>
2016-08-18 14:25:48 -0600

    add inventory to homepage

--- controllers/home_controller.js
+++ controllers/home_controller.js
@@ -2,4 +2,5 @@ app.controller('HomeController',function($scope, TeasService){
   $scope.vw = {};
   $scope.vw.teas = TeasService.all;
   $scope.vw.categories = categoriesArray(TeasService.all);
+  $scope.vw.quantities = [1,2,3,4,5,6];
 })
